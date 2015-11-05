28ce35b68e2dadf749b77b8216b862a0f4326005
wanghaohappy1 <wang.hao4@husky.neu.edu>
2015-11-04 19:06:33 -0500

    Update CastDao.java

--- src/model/CastDao.java
+++ src/model/CastDao.java
@@ -10,7 +10,6 @@ import javax.persistence.Persistence;
 import javax.persistence.Query;
 
 public class CastDao {
-
 	EntityManagerFactory factory=Persistence.createEntityManagerFactory("Movie");
 	public void createCast(int actorId, int movieId, Cast cast){
 		EntityManager em=factory.createEntityManager();
@@ -23,66 +22,54 @@ public class CastDao {
 		cast.setMovie(movie);
 		cast.setActor(actor);
 		em.merge(cast);
-		
 		actor.getCasts().add(cast);
 		movie.getCasts().add(cast);
 		em.merge(movie);
-        em.merge(actor);
-        
-        em.getTransaction().commit();
-        em.close();
+                em.merge(actor);
+                em.getTransaction().commit();
+                em.close();
 	}
 	
-	public void create3Cast(int actor1Id, int movie1Id, Cast cast1,
-			int actor2Id, int movie2Id, Cast cast2,
-			int actor3Id, int movie3Id, Cast cast3){
-		
+	public void create3Cast(int actor1Id, int movie1Id, Cast cast1, int actor2Id, int movie2Id, Cast cast2, 
+	        int actor3Id, int movie3Id, Cast cast3) {
 		EntityManager em=factory.createEntityManager();
 		em.getTransaction().begin();
 		em.persist(cast1);
 		
 		Actor actor1=em.find(Actor.class, actor1Id);
 		Movie movie1=em.find(Movie.class, movie1Id);
-		
 		cast1.setMovie(movie1);
 		cast1.setActor(actor1);
 		em.merge(cast1);
-		
 		actor1.getCasts().add(cast1);
 		movie1.getCasts().add(cast1);
 		em.merge(movie1);
-        em.merge(actor1);
-        
-        em.persist(cast2);
+                em.merge(actor1);
+                em.persist(cast2);
 		
 		Actor actor2=em.find(Actor.class, actor2Id);
 		Movie movie2=em.find(Movie.class, movie2Id);
-		
 		cast2.setMovie(movie2);
 		cast2.setActor(actor2);
 		em.merge(cast2);
-		
 		actor2.getCasts().add(cast2);
 		movie2.getCasts().add(cast2);
 		em.merge(movie2);
-        em.merge(actor2);
-        
-        em.persist(cast3);
+                em.merge(actor2);
+                em.persist(cast3);
 		
 		Actor actor3=em.find(Actor.class, actor3Id);
 		Movie movie3=em.find(Movie.class, movie3Id);
-		
 		cast3.setMovie(movie3);
 		cast3.setActor(actor3);
 		em.merge(cast3);
-		
 		actor3.getCasts().add(cast3);
 		movie3.getCasts().add(cast3);
 		em.merge(movie3);
-        em.merge(actor3);
+                em.merge(actor3);
         
-        em.getTransaction().commit();
-        em.close();
+                em.getTransaction().commit();
+                em.close();
 	}
 	
 	public List<Cast> listAllCasts()
@@ -104,12 +91,12 @@ public class CastDao {
 		List<Cast> casts = new ArrayList<Cast>();
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
-
 		casts = listAllCasts();
-		for (Cast cast: casts)
-		{
-			if (cast.getCharactorName().equals(character) )
+		
+		for (Cast cast : casts) {
+			if (cast.getCharactorName().equals(character)) {
 				check_character = true;
+			}
 		}
 
 		em.getTransaction().commit();
@@ -152,36 +139,4 @@ public class CastDao {
 		em.getTransaction().commit();
 		em.close();
 	}
-
-	public static void main(String[] args){
-		CastDao dao=new CastDao();
-		CastDao dao1=new CastDao();
-
-		
-		/*Cast cast=new Cast();
-		Cast cast1=new Cast();
-		Cast cast2=new Cast();
-
-
-		cast.setCharactorName("action actor");
-		cast1.setCharactorName("action actor111");
-		cast2.setCharactorName("action actor111");
-
-
-		dao.create3Cast(1, 1, cast, 1, 1, cast1,1,1,cast2);*/
-		System.out.println(dao.findcast("Jennie Carr"));
-		
-		//dao1.createCast(1, 2, cast1);
-		
-		/*cast=dao.getCast(1);
-		System.out.println(cast.getId());*/
-		
-		/*List<Cast> casts=dao.getCastForMovie(1);
-		
-		String newCharacter="action actor";
-		dao.changeCharacterForCast(1, newCharacter);*/
-	}
-
-
-
 }
