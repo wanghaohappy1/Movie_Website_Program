99422933db43c2aa23ea600321b1569cd036fc10
wanghaohappy1 <wang.hao4@husky.neu.edu>
2015-11-04 20:13:34 -0500

    Update LikeServlet.java

--- src/controller/LikeServlet.java
+++ src/controller/LikeServlet.java
@@ -19,46 +19,51 @@ import model.*;
 public class LikeServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	RequestDispatcher dispatcher = null;
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String username = request.getParameter("username");
 		String stars = request.getParameter("stars");
 		int movieId = Integer.parseInt(request.getParameter("movieId"));
-		
+
 		UserDao userDao = new UserDao();
 		int userId = userDao.findUserId(username);
 		User user = userDao.getUser(userId);
-		
+
 		ReviewDao reviewDao = new ReviewDao();
 		Review review = new Review();
-		
+
 		review.setUser(user);
 		review.setStar(stars);
-		reviewDao.createReview(userId,movieId,review);
-		
+		reviewDao.createReview(userId, movieId, review);
+
 		dispatcher = request.getRequestDispatcher("MovieDeveloper.jsp");
 		dispatcher.forward(request, response);
 	}
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LikeServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LikeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
