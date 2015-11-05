72ed9d6b1d51ad29ffe72334a8738f5992e6874b
wanghaohappy1 <wang.hao4@husky.neu.edu>
2015-11-04 20:16:03 -0500

    Update SearchServlet.java

--- src/controller/SearchServlet.java
+++ src/controller/SearchServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.*;
+
 /**
  * Servlet implementation class SearchServlet
  */
@@ -17,8 +18,9 @@ import model.*;
 public class SearchServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	RequestDispatcher dispatcher = null;
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String action = request.getParameter("action");
 		String title = request.getParameter("title");
 		String posterImage = request.getParameter("posterImage");
@@ -32,31 +34,35 @@ public class SearchServlet extends HttpServlet {
 		newMovie.setDescription(description);
 		newMovie.setRottenTomatoesId(rottenId);
 		MovieDao movieDao = new MovieDao();
-		
+
 		if ("add".equals(action)) {
 			movieDao.createMovie(newMovie);
 			dispatcher = request.getRequestDispatcher("Update.jsp");
 			dispatcher.forward(request, response);
-		} else if ("update".equals(action)){
+		} else if ("update".equals(action)) {
 			String id = request.getParameter("id");
 			int movieId = Integer.parseInt(id);
 			movieDao.updateMovie(movieId, newMovie);
 			dispatcher = request.getRequestDispatcher("Update.jsp");
-			dispatcher.forward(request, response);	 
+			dispatcher.forward(request, response);
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
