55374c895769e939b59040b2c6089adddd1ae801
wanghaohappy1 <wang.hao4@husky.neu.edu>
2015-11-04 19:22:09 -0500

    Update ReviewDao.java

--- src/model/ReviewDao.java
+++ src/model/ReviewDao.java
@@ -13,12 +13,11 @@ import model.*;
 public class ReviewDao {
 	EntityManagerFactory factory=Persistence.createEntityManagerFactory("Movie");
 	
-	
-	public void createReview(int userId, int movieId, Review review){
+	public void createReview(int userId, int movieId, Review review) {
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
-
 		em.persist(review);
+		
 		User user = em.find(User.class, userId);
 		Movie movie = em.find(Movie.class, movieId);
 		
@@ -34,11 +33,9 @@ public class ReviewDao {
 		
 		em.getTransaction().commit();
 		em.close();
-        
 	}
 	
-	public void likeMovie(int userId, Movie movie)
-	{
+	public void likeMovie(int userId, Movie movie) {
 		EntityManager em=factory.createEntityManager();
 		em.getTransaction().begin();
 		
@@ -46,19 +43,19 @@ public class ReviewDao {
 		review.setStar("5");
 		MovieDao movieDao = new MovieDao();
 		boolean checkMovie = movieDao.checkMovie(movie.getRottenTomatoesId());
-		if(!checkMovie)
-		{
+		
+		if (!checkMovie) {
 			movieDao.createMovie(movie);
 		}
-		int movieId = movieDao.findMovieIdForRottenTomatoesId(movie.getRottenTomatoesId());
 		
+		int movieId = movieDao.findMovieIdForRottenTomatoesId(movie.getRottenTomatoesId());
 		createReview(userId,movieId,review);
 		
 		em.getTransaction().commit();
 		em.close();
 	}
 	
-	public Review getReview(int reviewId){
+	public Review getReview(int reviewId) {
 		EntityManager em=factory.createEntityManager();
 		em.getTransaction().begin();
 		
@@ -69,8 +66,7 @@ public class ReviewDao {
 		return review;
 	}
 	
-	public List<Review> listAllReviews()
-	{
+	public List<Review> listAllReviews() {
 		List<Review> reviews = new ArrayList<Review>();
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
@@ -83,8 +79,7 @@ public class ReviewDao {
 		return reviews;
 	}
 	
-	public List<Review> listNReviewsForMovie(int n, int movieId)
-	{
+	public List<Review> listNReviewsForMovie(int n, int movieId) {
 		List<Review> reviews = new ArrayList<Review>();
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
@@ -93,31 +88,30 @@ public class ReviewDao {
 		if(allReviews.size()<n)
 			n=allReviews.size();
 		int i = n;
-		for(int k= allReviews.size()-1; k >= 0 ; k--)
-		{
-			if(movieId == allReviews.get(k).getMovie().getId())
-				if(i > 0)
-				{
+		for(int k= allReviews.size()-1; k >= 0 ; k--) {
+			if(movieId == allReviews.get(k).getMovie().getId()) {
+				if(i > 0) {
 					reviews.add(allReviews.get(k));
 					i--;
 				}
+			}
 		}
 		
 		em.getTransaction().commit();
 		em.close();
 		return reviews;
 	}
-
 	
 	public void updateReview(int reviewId, Review review){
-		EntityManager em=factory.createEntityManager();
+		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
 		
-		Review reviewed=em.find(Review.class, reviewId);
+		Review reviewed = em.find(Review.class, reviewId);
 		reviewed.setReviews(review.getReviews());
 		reviewed.setDateOfReview(review.getDateOfReview());
 		reviewed.setStar(review.getStar());
 		em.merge(reviewed);
+		
 		em.getTransaction().commit();
 		em.close();
 	}
@@ -127,12 +121,12 @@ public class ReviewDao {
 		List<Review> reviews = new ArrayList<Review>();
 		EntityManager em = factory.createEntityManager();
 		em.getTransaction().begin();
-
 		List<Review> allReviews = listAllReviews();
-		for(Review review: allReviews)
-		{
-			if(userId == review.getUser().getId() && review.getStar().equals("5"))
+		
+		for(Review review: allReviews) {
+			if(userId == review.getUser().getId() && review.getStar().equals("5")) {
 				reviews.add(review);
+			}
 		}
 		
 		em.getTransaction().commit();
@@ -145,31 +139,11 @@ public class ReviewDao {
 		em.getTransaction().begin();
 
 		Review review = em.find(Review.class, reviewId);
-        
-        User user=review.getUser();
+                User user=review.getUser();
 		Movie movie=review.getMovie();
-		
 		em.remove(review);
 
 		em.getTransaction().commit();
 		em.close();
-	}
-	public static void main(String[] args) {
-		ReviewDao dao = new ReviewDao();
-		
-		Review review = new Review();
-		review.setReviews("aaa");
-		
-		Review review1 = new Review();
-		review1.setReviews("aaa");
-		
-		dao.createReview(18, 2, review1);
-		dao.createReview(18, 2, review);
-		List<Review> reviews = dao.listAllReviews();
-		for (Review review2: reviews)
-		{
-			System.out.println(review2.getReviews());
-		}
-
 	}
 }
